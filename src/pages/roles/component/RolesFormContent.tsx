39129d8b3c4773461eb225b97f8554045ede25cc
epaphrasagyapong <epaphrasagyapong1@gmail.com>
2026-02-06 00:31:52 +0000

    -

--- src/pages/roles/component/RolesFormContent.tsx
+++ src/pages/roles/component/RolesFormContent.tsx
@@ -49,7 +49,7 @@ const RolesFormContent = ({ isLoading, form, }: IField) => {
           <>
             <div className="flex items-center justify-between gap-2 mb-2">
               <p className="flex items-center gap-2">
-                <BookOpenText className="w-4 h-4" /> Role Basic Information
+                <BookOpenText className="w-4 h-4" /> Basic Role Information
               </p>
               <p className="text-xs text-rx-secondary">
                 Required Information <span className="text-red-500">*</span>
