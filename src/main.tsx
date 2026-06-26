12e8802226fd550b5fedfa497cdacbe7e1a36fea
Eliezer <eliezer.somuah.es@gmail.com>
2026-06-26 09:13:37 +0000

    feat: implemented PWA for desktop installation

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
 import LoadingComponent from "./components/LoadingComponent.tsx";
+
+registerSW({ immediate: true });
 
 const AppRoutes = lazy(() => import("./routes/AppRoutes.tsx"));
 
